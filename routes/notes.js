946299d6acf4e5643c423016e5050bdf20644736
unknown <kosukematsuz@yahoo.co.jp>
2024-06-02 17:54:16 +0900

    add mongodb

--- routes/notes.js
+++ routes/notes.js
@@ -1,33 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-//response data
-const responseObjectDataAll ={
-    textObject :{
-        id:1,
-        title:'ノート１のタイトル',
-        subTitle:'ノート１のサブタイトル',
-        bodyText:'ノート１の本文'
-    },
-    textObject2 :{
-        id:2,
-        title:'ノート2のタイトル',
-        subTitle:'ノート2のサブタイトル',
-        bodyText:'ノート2の本文'
-    },
-};
 
-/**
- * メモを全権取得するAPI
- * @returns{Object[]}data
- * @returns{number}data.id - ID
- * @returns{string}data.title - タイトル
- * @returns{string}data.text - 内容
- */
 
-router.get('/',function(req,res,next){
-    //全件取得して返す
-    res.json(responseObjectDataAll);
+
+// 接続情報を設定
+const{ MongoClient} = require("mongodb");
+const uri= "";
+const client= new MongoClient(uri);
+
+router.get('/', async(req, res) =>{
+// データベース、コレクションを指定
+const database= client.db('notes');
+const notes= database.collection('notes');
+// idが１のドキュメントを取得
+const query= { id:2};
+const note= await notes.findOne(query);
+res.json(note);
 })
-
 module.exports = router;
